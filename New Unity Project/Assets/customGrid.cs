4d09f0924f53c9f802bb3ac321baf70fdae49fcb
Joseph Byam <jmbyam@alaska.edu>
2019-02-14 11:42:43 -0900

    The grid changes

--- New Unity Project/Assets/customGrid.cs
+++ New Unity Project/Assets/customGrid.cs
@@ -11,10 +11,11 @@ public class customGrid : MonoBehaviour
 
     void LateUpdate()
     {
-        truePos.x = Mathf.Floor(target.transform.position.x / gridSize) = gridSize;
-        truePos.y = Mathf.Floor(target.transform.position.y / gridSize) = gridSize;
-        truePos.z = Mathf.Floor(target.transform.position.z / gridSize) = gridSize;
+        truePos.x = Mathf.Floor(target.transform.position.x / gridSize) * gridSize;
+        truePos.y = Mathf.Floor(target.transform.position.y / gridSize) * gridSize;
+        truePos.z = Mathf.Floor(target.transform.position.z / gridSize) * gridSize;
 
         structure.transform.position = truePos;
     }
 }
+
