65dccf7636d4720afa793499f40c2ab913506bc1
Joseph Byam <jmbyam@alaska.edu>
2019-02-14 12:53:02 -0900

    Making ship cubes to move around

--- New Unity Project/Assets/customGrid.cs
+++ New Unity Project/Assets/customGrid.cs
@@ -4,36 +4,54 @@ using UnityEngine;
 
 public class customGrid : MonoBehaviour
 {
-    public GameObject target;
-    public GameObject structure;
-    Vector3 truePos;
     public float gridSize;
-
+    public List<Ship> ships;
+    
+    private void Start()
+    {
+        Ship carrier = new Ship(5);
+        Ship battleship = new Ship(4);
+        Ship cruiser = new Ship(3);
+        Ship submarine = new Ship(3);
+        Ship destroyer = new Ship(2);
+        ships = new List<Ship>();
+        ships.Add(carrier);
+        ships.Add(battleship);
+        ships.Add(cruiser);
+        ships.Add(submarine);
+        ships.Add(destroyer);
+    }
     void LateUpdate()
     {
-        truePos.x = Mathf.Floor(target.transform.position.x / gridSize) * gridSize;
-        truePos.y = Mathf.Floor(target.transform.position.y / gridSize) * gridSize;
-        truePos.z = Mathf.Floor(target.transform.position.z / gridSize) * gridSize;
+        for (int i = 0; i < ships.Count; ++i)
+        {
+            GameObject target = ships[i].model;
+            Vector3 truePos = target.transform.position;
+            truePos.x = Mathf.Floor(target.transform.position.x / gridSize) * gridSize;
+            truePos.y = Mathf.Floor(target.transform.position.y / gridSize) * gridSize;
+            truePos.z = Mathf.Floor(target.transform.position.z / gridSize) * gridSize;
 
-        if (truePos.x < 0)
-        {
-            truePos.x = 0;
-        }
-        else if ( truePos.x > 10 )
-        {
-            truePos.x = 10;
+            if (truePos.x < 0)
+            {
+                truePos.x = 0;
+            }
+            else if (truePos.x > 10)
+            {
+                truePos.x = 10;
+            }
+
+            if (truePos.z < 0)
+            {
+                truePos.z = 0;
+            }
+            else if (truePos.z > 10)
+            {
+                truePos.z = 10;
+            }
+
+            target.transform.position = truePos;
         }
 
-        if (truePos.z < 0)
-        {
-            truePos.z = 0;
-        }
-        else if (truePos.z > 10)
-        {
-            truePos.z = 10;
-        }
-
-        structure.transform.position = truePos;
     }
 }
 
---
+++ New Unity Project/Assets/ship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public GameObject model;
+
+    public int size;
+    ArrayList hits;
+
+    public Ship(int size) {
+        model = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        model.transform.localScale = new Vector3(size, 1, 1);
+        this.size = size;
+        hits = new ArrayList();
+
+        for (int i = 0; i < size; ++i)
+        {
+            hits.Add(false);
+        }
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
+
