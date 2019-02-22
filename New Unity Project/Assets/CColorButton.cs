a7d2d1f11c415dafd945091dc840c954a8c6bdaa
Joseph Byam <jmbyam@alaska.edu>
2019-02-22 12:12:01 -0900

    Made 98 button that turn white when clicked on

---
+++ New Unity Project/Assets/CColorButton.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CColorButton : MonoBehaviour
+{
+    public Button button;
+
+    private bool buttonFlag = false;
+    private int index = 1;
+
+    public void buttonPressed()
+    {
+    
+        button.colors = UnityEngine.UI.ColorBlock.defaultColorBlock;
+        buttonFlag = false;
+
+    }
+}
